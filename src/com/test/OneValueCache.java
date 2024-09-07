5ce742e9176706ce42d0c8cb7ab201b081e610d6
Vasiliy <Mst.Vasek999@yandex.ru>
2024-09-07 14:49:26 +0200

    3.4.2 Немутированный держатель для кэширования числа и его множителей

---
+++ src/com/test/OneValueCache.java
@@ -0,0 +1,21 @@
+package src.com.test;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class OneValueCache {
+    private final BigInteger lastNumber;
+    private final BigInteger[] lastFactors;
+
+    public OneValueCache(BigInteger i, BigInteger[] factors) {
+        this.lastNumber = i;
+        this.lastFactors = Arrays.copyOf(factors, factors.length);
+    }
+    public BigInteger[] getFactors(BigInteger i) {
+        if (lastNumber == null || !lastNumber.equals(i)){
+            return null;
+        }
+        else
+            return Arrays.copyOf(lastFactors, lastFactors.length);
+    }
+}
