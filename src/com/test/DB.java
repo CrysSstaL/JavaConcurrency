1d4b6ec7950e07f9a85964362f29760081896748
Vasiliy <Mst.Vasek999@yandex.ru>
2024-09-07 14:20:03 +0200

    3.3.3 ThreadLocal. Использование класса ThreadLocal для ограничения одним потоком

---
+++ src/com/test/DB.java
@@ -0,0 +1,22 @@
+package src.com.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB {
+    static String DB_URL = "";
+    public static ThreadLocal<Connection> connectionHolder
+            = new ThreadLocal<Connection>(){
+        public Connection initialValue(){
+            try {
+                return DriverManager.getConnection(DB_URL);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    };
+    public static Connection getConnection(){
+        return connectionHolder.get();
+    }
+}
